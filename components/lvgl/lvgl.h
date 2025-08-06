84dfe518284021eee62ae06ee04e6c8c1d83db87
novaeco <novatribolnovea@gmail.com>
2025-08-06 11:21:17 +0200

    Correction de la structure du composant LVGL

---
+++ components/lvgl/lvgl.h
@@ -0,0 +1,14 @@
+/**
+ * @file lvgl.h
+ * @brief Fichier principal LVGL pour NovaReptileElevage
+ * 
+ * Ce fichier redirige vers le vrai header LVGL
+ */
+
+#ifndef LVGL_H
+#define LVGL_H
+
+// Inclusion du fichier LVGL principal
+#include "lvgl/lvgl.h"
+
+#endif // LVGL_H
