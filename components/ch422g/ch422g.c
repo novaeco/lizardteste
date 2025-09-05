ae41ace88e7bb43ac0298355253bc91af345a383
novaeco <novatribolnovea@gmail.com>
2025-09-05 15:33:29 +0200

    feat: add shared I2C bus component

--- components/ch422g/ch422g.c
+++ components/ch422g/ch422g.c
@@ -7,10 +7,8 @@
 #include "driver/i2c_master.h"
 #include "esp_log.h"
 #include "esp_check.h"
+#include "i2c_bus.h"
 
-#define CH422_I2C_PORT I2C_NUM_1
-#define CH422_PIN_SDA 4
-#define CH422_PIN_SCL 5
 #define CH422_I2C_ADDR 0x20
 #define CH422_I2C_FREQ 100000
 
@@ -31,18 +29,8 @@ esp_err_t ch422g_init(void)
         return ESP_OK;
     }
 
-    i2c_master_bus_config_t bus_conf = {
-        .i2c_port = CH422_I2C_PORT,
-        .sda_io_num = CH422_PIN_SDA,
-        .scl_io_num = CH422_PIN_SCL,
-        .clk_source = I2C_CLK_SRC_DEFAULT,
-        .glitch_ignore_cnt = 7,
-        .flags = {
-            .enable_internal_pullup = true,
-        },
-    };
-
-    ESP_RETURN_ON_ERROR(i2c_new_master_bus(&bus_conf, &s_bus), TAG, "bus init");
+    s_bus = i2c_bus_get();
+    ESP_RETURN_ON_FALSE(s_bus, ESP_ERR_INVALID_STATE, TAG, "bus not init");
 
     i2c_device_config_t dev_conf = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
